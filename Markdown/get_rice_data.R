868169f1e8b0470991f5564096cf85283be02dd9
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 <19jbarnett3289@gmail.com>
2024-10-15 21:35:15 -0400

    changed it

---
+++ Markdown/get_rice_data.R
@@ -0,0 +1,36 @@
+getRiceData <- function() {
+  
+  library(tidyverse)
+  library(knitr)
+  library(kableExtra)
+  
+  url <- "https://docs.google.com/spreadsheets/d/1Mk1YGH9LqjF7drJE-td1G_JkdADOU0eMlrP01WFBT8s/pub?gid=0&single=true&output=csv"
+  
+  rice <- read_csv(url)
+  
+  result <- rice |>
+    # Get rid of extra data (only choose the variables we want)
+    select(DateTime, H2O_TempC, AirTempF, PH, WindDir, Rain_in, 
+           Turbidity_ntu, SpCond_mScm, ODO_sat, Depth_m, Chla_ugl) |>
+    # Make date object
+    mutate(Date = mdy_hms(DateTime, tz = "EST")) |>
+    mutate(AirTemp = (AirTempF - 32) * 5 / 9) |>
+    mutate(Rain_m = Rain_in*0.0254) |>
+    # Make Weekday, day, and month object
+    mutate(Weekday = wday(Date, label = TRUE, abbr = FALSE)) |>
+    mutate(month = month(Date, label = TRUE, abbr = FALSE)) |>
+    mutate(day = day(Date)) |>
+    select(Date, month, day, Weekday, AirTemp, Rain_m,PH, WindDir, 
+           Turbidity_ntu, SpCond_mScm, ODO_sat, Depth_m, Chla_ugl)
+ 
+  
+#returns the result of the data
+  
+  return(result)
+}
+
+
+
+  
+
+
