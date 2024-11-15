7fca9051fd6efa6f1e71e2e0001f855cbca8d9e7
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 <19jbarnett3289@gmail.com>
2024-11-14 19:45:10 -0500

    hw commit

--- Markdown/get_rice_data.R
+++ Markdown/get_rice_data.R
@@ -11,17 +11,18 @@ getRiceData <- function() {
   result <- rice |>
     # Get rid of extra data (only choose the variables we want)
     select(DateTime, H2O_TempC, AirTempF, PH, WindDir, Rain_in, 
-           Turbidity_ntu, SpCond_mScm, ODO_sat, Depth_m, Chla_ugl, RelHumidity, BP_HG) |>
+           Turbidity_ntu, SpCond_mScm, ODO_sat, Depth_m, Chla_ugl, RelHumidity, BP_HG, WindSpeed_mph) |>
     # Make date object
     mutate(Date = mdy_hms(DateTime, tz = "EST")) |>
     mutate(AirTemp = (AirTempF - 32) * 5 / 9) |>
     mutate(Rain_m = Rain_in*0.0254) |>
+    mutate(WindSpeed_kph = WindSpeed_mph*1.609344) |>
     # Make Weekday, day, and month object
     mutate(Weekday = wday(Date, label = TRUE, abbr = FALSE)) |>
     mutate(month = month(Date, label = TRUE, abbr = FALSE)) |>
     mutate(day = day(Date)) |>
     select(Date, month, day, Weekday, AirTemp, Rain_m,PH, WindDir, 
-           Turbidity_ntu, SpCond_mScm, ODO_sat, Depth_m, Chla_ugl, RelHumidity, BP_HG)
+           Turbidity_ntu, SpCond_mScm, ODO_sat, Depth_m, Chla_ugl, RelHumidity, BP_HG, WindSpeed_kph)
  
   
 #returns the result of the data
